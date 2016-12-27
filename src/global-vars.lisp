6802b6c635906039116df4a782611554414057c0
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2016-12-28 03:58:55 +0530

    Added globals variables to state track of player.

---
+++ src/global-vars.lisp
@@ -0,0 +1,11 @@
+;;; Variables
+;; Player State
+(defparameter *player-health* nil)
+(defparameter *player-strength* nil)
+(defparameter *player-agility* nil)
+
+;; Monsters
+
+(defparameter *monsters* nil)
+(defparameter *monster-builders* nil)
+(defparameter *monster-num* 12)
